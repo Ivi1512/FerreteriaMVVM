3bef4d425616a958218d3f1c3ae446af0fdc9857
Ivi1512 <ivandelafuentemeco@gmail.com>
2021-11-25 18:29:50 +0100

    Agregar archivos de proyecto.

---
+++ FerreteriaMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerreteriaMVVM.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
